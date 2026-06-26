a2fde2d761e48624e54f5bd8cce160222ef5a443
Your Name <bilalzainab123agmail.com>
2026-06-26 22:08:10 +0500

    added Iink in the axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://mern-deploy-production-4279.up.railway.app',
     headers: { 'Content-Type': 'application/json' }
 });
 
